ba22d430eb45bf49e704d9113aa522b3c0b6cafc
David Gonzalez <davidgonzalez@MacBook-Pro-de-David.local>
2021-07-24 17:32:08 -0500

    Fresh Start

---
+++ src/test/java/gon/cue/springbootbackendapirest/SpringBootBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package gon.cue.springbootbackendapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
